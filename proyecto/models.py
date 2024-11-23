7b9777a6d5785b91b39091df4e364c4854411554
898ronin <greenkappa16@gmail.com>
2024-11-22 21:43:38 -0300

    primer commit

---
+++ proyecto/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Proyecto(models.Model):
+    nombre_proyecto = models.CharField(max_length=150)
+    rut = models.CharField(max_length=150)
+    primer_nombre = models.CharField(max_length=150)
+    segundo_apellido = models.CharField(max_length=150)
