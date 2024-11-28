9a7faf29a0fd8d8cff3397a73c13b4390345defa
898ronin <greenkappa16@gmail.com>
2024-11-28 15:33:48 -0300

    Push FInal

--- proyecto/api.py
+++ proyecto/api.py
@@ -1,8 +1,17 @@
-from proyecto.models import Proyecto
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from .models import Proyecto
 from .serializers import ProyectoSerializer
 
 class ProyectoViewSet(viewsets.ModelViewSet):
     queryset = Proyecto.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.AllowAny] 
     serializer_class = ProyectoSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def perform_destroy(self, instance):
+        instance.delete()
--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from . import views
+from .api import ProyectoViewSet
 
 urlpatterns = [
+    # Rutas HTML
     path('', views.index, name='index'),
     path('proyectos/', views.proyecto_list, name='proyecto_list'),
     path('proyectos/new/', views.proyecto_create, name='proyecto_create'),
     path('proyectos/<int:pk>/edit/', views.proyecto_update, name='proyecto_update'),
     path('proyectos/<int:pk>/delete/', views.proyecto_delete, name='proyecto_delete'),
+
+    # Rutas de API
+    path('api/proyectos/', ProyectoViewSet.as_view({'get': 'list'}), name='proyecto_api_list'),
+    path('api/proyectos/<int:pk>/', ProyectoViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name='proyecto_api_detail'),
 ]
+
