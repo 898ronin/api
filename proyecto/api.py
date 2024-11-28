2d17b5937f74d851b5a8d4ae187138170fc23644
898ronin <greenkappa16@gmail.com>
2024-11-28 14:18:03 -0300

    Rollback 2

---
+++ proyecto/api.py
@@ -0,0 +1,8 @@
+from proyecto.models import Proyecto
+from rest_framework import viewsets, permissions
+from .serializers import ProyectoSerializer
+
+class ProyectoViewSet(viewsets.ModelViewSet):
+    queryset = Proyecto.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProyectoSerializer
