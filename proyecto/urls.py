eab96d254e8861577d25dd121e432ae681abd6cd
898ronin <greenkappa16@gmail.com>
2024-11-28 14:16:05 -0300

    test 1

--- proyecto/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .api import ProyectoViewSet
-
-router = routers.DefaultRouter()
-
-router.register("api/proyecto", ProyectoViewSet, "proyecto")
-
-urlpatterns = router.urls
